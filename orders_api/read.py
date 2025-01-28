be50f1f6a16b9e04c4bc16136287f6ed8ebe7973
Chelsey Zhang <chelsey.zcy@gmail.com>
2025-01-29 10:39:41 +1100

    add output section

--- orders_api/read.py
+++ orders_api/read.py
@@ -3,9 +3,10 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key
 
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ.get('ORDERS_TABLE')
+
 def lambda_handler(event, context):
-  dynamodb = boto3.resource('dynamodb')
-  table_name = os.environ.get('ORDERS_TABLE')
   table = dynamodb.Table(table_name)
   order_id = event['pathParameters']['id']
   path_params = event.get('pathParameters', {})
